951d29d7da65f6aeec47742ea204ece90b3c5e29
lidianvilazio <lidianvilazio@hotmail.com>
2018-05-07 11:57:14 -0400

    l

--- books-system-frontend/src/Components/Login.js
+++ books-system-frontend/src/Components/Login.js
@@ -15,42 +15,25 @@ class Login extends React.Component {
   // <button type="submit" className="btn btn-success btn-block" onSubmit={this.handleSubmit} value="Submit">Submit</button>
   // </form>
 
-  DEFAULT_STATE = {
+  state = {
     username: "",
-    password: "",
-    errors: []
-  }
-  state = this.DEFAULT_STATE
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { username, password } = this.state;
-    const auth = { username, password };
-    this.doFetch(auth)
+    password: ""
   }
 
-  authSet = (auth) =>{
-      localStorage.auth = JSON.stringify(auth);
-      this.setState({ auth });
-    }
-
-  doFetch = (auth) => {
-    fetch("http://localhost:3000/login", {
-      method: "POST",
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("HELLO!!!!")
+    fetch("http://localhost:3000/sessions", {
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        "Accept": "application/javascript"
       },
-      body: JSON.stringify(auth)
-    }).then(r => r.json()).then(json => {
-      if (json.error){
-        this.setState({errors: json.error})
-      } else {
-        this.setState({ ...this.DEFAULT_STATE })
-        this.authSet(json)
-        this.history.push("/")
-      }
-    })
+      method: "POST",
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    }).then(r => r.json()).then(console.log)
   }
 
   handleChange = (e) => {
@@ -59,13 +42,10 @@ class Login extends React.Component {
     })
   }
 
-  
-
 render(){
   console.log("username"+this.state.username, "pw"+this.state.password)
   return(
     <div className="col-md-6 mb-4">
-    {this.state.errors.length ? alert({this.state.errors}) : ""}
                     <div className="card">
                         <div className="card-body">
                             <h3 className="text-center default-text py-3"><i className="fa fa-lock"></i> Login:</h3>
@@ -82,15 +62,11 @@ render(){
                                 <label for="defaultForm-pass">Your password</label>
                             </div>
                             <div className="text-center">
-                                <button className="btn btn-default waves-effect waves-light" onClick={this.handleSubmit}>Submit</button>
+                                <button className="btn btn-default waves-effect waves-light" onSubmit={this.handleSubmit}>Submit</button>
                             </div>
                             </div>
                             </div>
-
-
-
-
-
+                            
                             </div>
 
   )
